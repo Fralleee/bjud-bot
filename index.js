d8044d8a6c0fcfe7d74341e5a5996d2a3c73449a
Roland Chelwing <jolleee@hotmail.com>
2022-09-20 10:59:10 +0200

    Initial

---
+++ index.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import { Client, GatewayIntentBits } from "discord.js";
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers, GatewayIntentBits.DirectMessages],
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
+client.on("messageCreate", async message => {
+  console.log(message);
+});
