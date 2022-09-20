9df3e2e569c77cad092fa21838c66776c2fa0815
Roland Chelwing <jolleee@hotmail.com>
2022-09-20 11:53:32 +0200

    Changes

--- index.js
+++ index.js
@@ -2,6 +2,30 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import { Client, GatewayIntentBits } from "discord.js";
+import express from "express";
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  console.log(
+    JSON.stringify({
+      subject: "Leetcode Rank",
+      status: "leetcode",
+      color: "orange",
+    })
+  );
+
+  res.json({
+    subject: "leetcode",
+    status: "814,050",
+    color: "orange",
+  });
+});
+// app.use('/leetcode', leetcode);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers, GatewayIntentBits.DirectMessages],
