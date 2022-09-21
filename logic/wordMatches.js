6969d34bf6470e11b8a8f5274c81ea5bad083f89
Roland Chelwing <jolleee@hotmail.com>
2022-09-21 12:03:49 +0200

    Changes

--- logic/wordMatches.js
+++ logic/wordMatches.js
@@ -22,7 +22,7 @@ export function wordMatches(content) {
     return "Håll om mig!";
   }
   if (majkstyle.some(val => content.includes(val))) {
-    return "**Majk special**\n*Se även: lat, soffpotatis, alkoholist* \n*(Uttal IPA: /maɪkstaɪl/)* \nHandlingen där datorspelaren hämtar __två__ öl istället för __en__.";
+    return "**Majk special**\n*Se även: lat, soffpotatis, alkoholist* \n*(Uttal IPA: /maɪkspɛʃ.əl/)* \nHandlingen där datorspelaren hämtar __två__ öl istället för __en__.";
   }
 
   if (content.includes("värsåsjönna")) return "VärsåsJönna <3";
