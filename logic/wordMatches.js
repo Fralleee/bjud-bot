ccf27e3624a21fd9cc18eda92b305045aa529fa9
Roland Chelwing <jolleee@hotmail.com>
2023-07-03 17:57:34 +0200

    Update logic

--- logic/wordMatches.js
+++ logic/wordMatches.js
@@ -1,53 +1,50 @@
-const boring = ["salem", "jönköping"];
-const wussy = ["sova", "söva", "ge mig"];
-const bjud = ["bjuds det", "om det bjuds", "dåligt med bjud"];
-const gotta = ["öl", "kebab", "pizza", "taco", "korka upp"];
-const gun = ["hallåses", "fel på låset", "fel på låses", "brevinkastet", "låna wc"];
-const majkstyle = ["majkstyle", "dra en majk", "majk-style", "majk special", "majk-special", "majkspecial"];
-const tack = ["tack", "tock", "takk"];
-const daensk = ["böge", "bestefar", "kuke"];
-const douche = ["easy game", "easy life", "ez game"];
-const brunt = ["brunt ut", "aningen brunt", "mycket brunt", "väldigt brunt"];
-const jordkällare = ["jordkällare", "fritzl", "fritzel", "linder"];
+import fuzzysort from "fuzzysort";
+
+const keywordMap = new Map([
+  [["salem", "jönköping"], "Näee va tråkigt."],
+  [["sova", "söva", "ge mig"], "Klent."],
+  [["bjuds det", "om det bjuds", "dåligt med bjud"], "Klart det bjuds"],
+  [["öl", "kebab", "pizza", "taco", "korka upp"], "Ååh va gott!"],
+  [["hallåses", "fel på låset", "fel på låses", "brevinkastet", "låna wc"], "Håll om mig!"],
+  [
+    ["majkstyle", "dra en majk", "majk-style", "majk special", "majk-special", "majkspecial"],
+    "**Majk special** *[maɪkspɛʃ.əl]*\nHandlingen där datorspelaren hämtar __två__ öl istället för __en__.\n*Se även: lat, soffpotatis, alkoholist*",
+  ],
+  [["tack", "tock", "takk"], "Naj naj naj, RU sa ha tack!"],
+  [["böge", "bestefar", "kuke"], "Jæ meget godt!"],
+  [["easy game", "easy life", "ez game"], "Och en glädjelig jul till er alla!"],
+  [["brunt ut", "aningen brunt", "mycket brunt", "väldigt brunt"], "Ser rätt brunt ut ja."],
+  [["jordkällare", "fritzl", "fritzel", "linder"], "Micke?"],
+  [["brifelt", "linus"], "Ta inte för givet att din kamrat står vid punkten!"],
+]);
 
 export function wordMatches(content) {
-  if (bjud.some(val => content.includes(val))) {
-    return "Klart det bjuds";
-  }
-  if (majkstyle.some(val => content.includes(val))) {
-    return "**Majk special** *[maɪkspɛʃ.əl]*\nHandlingen där datorspelaren hämtar __två__ öl istället för __en__.\n*Se även: lat, soffpotatis, alkoholist*";
-  }
-  if (gotta.some(val => content.includes(val))) {
-    return "Ååh va gott!";
-  }
-  if (boring.some(val => content.includes(val))) {
-    return "Näee va tråkigt.";
-  }
-  if (wussy.some(val => content.includes(val))) {
-    return "Klent.";
-  }
-  if (gun.some(val => content.includes(val))) {
-    return "Håll om mig!";
-  }
-  if (tack.some(val => content.includes(val))) {
-    return "Naj naj naj, RU sa ha tack!";
-  }
-  if (daensk.some(val => content.includes(val))) {
-    return "Jæ meget godt!";
-  }
-  if (douche.some(val => content.includes(val))) {
-    return "Och en glädjelig jul till er alla!";
-  }
-  if (brunt.some(val => content.includes(val))) {
-    return "Ser rätt brunt ut ja.";
-  }
-  if (jordkällare.some(val => content.includes(val))) {
-    return "Micke?";
+  if (content.startsWith("/tagga")) {
+    let game = content.slice("/tagga".length).trim();
+    if (game) {
+      return `Woooh! Let's play ${game}!`;
+    }
   }
 
-  if (content.includes("öl?")) return "När får man hämta öl egentligen?";
-  if (content.includes("öl!")) return "Nu får man hämta öl!!!";
-  if (content.includes("värsåsjönna")) return "VärsåsJönna <3";
-  if (content === "spel?") return "Dans!";
-  if (content === "dans?") return "Spel!";
+  for (let [words, response] of keywordMap.entries()) {
+    for (let word of words) {
+      let result = fuzzysort.single(word, content);
+      if (result && result.score > -5000) {
+        return response;
+      }
+    }
+  }
+
+  switch (content) {
+    case "öl?":
+      return "När får man hämta öl egentligen?";
+    case "öl!":
+      return "Nu får man hämta öl!!!";
+    case "värsåsjönna":
+      return "VärsåsJönna <3";
+    case "spel?":
+      return "Dans!";
+    case "dans?":
+      return "Spel!";
+  }
 }
